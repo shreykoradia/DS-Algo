c0c8f44031a61a5386bb5cbea47438bc78fb7ef5
shreykoradia <shrey.koradia@yahoo.com>
2021-08-12 12:40:51 +0530

    My first DS code

---
+++ Array/array.java
@@ -0,0 +1,21 @@
+package Array;
+
+import java.util.*;
+
+ class array {
+    public static void main(String[] args) {
+        int[] priceofPen = new int[5];
+        Scanner sc = new Scanner(System.in);
+        
+            for(int i = 0 ; i<priceofPen.length; i++){
+                System.out.println("Enter the price of Pen");
+                priceofPen[i] = sc.nextInt();
+            }
+
+            for(int i = 0 ; i<priceofPen.length; i++){
+                System.out.println("The price of the pen is " + priceofPen[i] + " ");
+               
+            }
+        sc.close();
+    }
+}
