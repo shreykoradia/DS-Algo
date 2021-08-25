f2abca4010a13c00b9ae22cfe3733d56664b5e91
shreykoradia <shrey.koradia@yahoo.com>
2021-08-25 08:07:33 +0530

    approach number 1 more than 1 time

--- Array/MajorityElement.java
+++ Array/MajorityElement.java
@@ -1,18 +1,38 @@
 package Array;
-
 import java.util.Scanner;
 
-public class MajorityElement {
+ class MajorityElement {
+            void majorityElement(int[] arr){
+                    int count = 0 ;
+                    int tem = 0;
+                for(int i = 0 ; i < arr.length; i++){
+                    for(int j =arr.length-1 ; j>=0 ; j--){
+                    if(arr[i] == arr[j]){
+                        count = count++ ;
+                         tem = arr[i];
+                        }
+                    }
+                }
+                System.out.println(tem);
+            }
+
+
     public static void main(String[] args) {
-        int index ; 
-        System.out.println("Enter the values of the indexs");
+        MajorityElement ob = new MajorityElement();
+         int n ; 
+        System.out.println("Enter the values of the n");
         Scanner sc = new Scanner(System.in);
-        index  = sc.nextInt();
-        int[] arr = new int[index] ;
+        n  = sc.nextInt();
+        int[] arr = new int[n] ;
         for(int i = 0 ; i < arr.length ; i++){
             System.out.println("Enter the value of the array in order ");
             arr[i] = sc.nextInt();
         }
+        ob.majorityElement(arr);
         sc.close();
     }
 }
+
+
+
+// this is a leet code question the question no is 229 henceforth if any more doubt in the question read it on leetcode.com
